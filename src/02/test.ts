1b234be158b36a7a1d368abb5c084ae2ee941ecb
Sergio Cinos <sergio.cinos@gmail.com>
2020-12-02 06:14:26 +0100

    Day 02

---
+++ src/02/test.ts
@@ -0,0 +1,26 @@
+import { strict as assert } from "assert";
+import { part1, part2 } from ".";
+import { readInput } from "../lib/readInput";
+
+describe("Day 2", () => {
+  it("Part 1", () => {
+    const input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    assert.equal(part1(input), "2");
+  });
+
+  it("Part 2", () => {
+    const input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    assert.equal(part2(input), "1");
+  });
+
+  describe("Solutions", () => {
+    it("Part 1", async () => {
+      const input = await readInput("02");
+      assert.equal(part1(input), "638");
+    });
+    it("Part 2", async () => {
+      const input = await readInput("02");
+      assert.equal(part2(input), "699");
+    });
+  });
+});
--- src/run.ts
+++ src/run.ts
@@ -2,12 +2,14 @@ import { promises as fs } from "fs";
 
 // Explicit imports for easier static analysis
 import * as solverDay01 from "./01";
+import * as solverDay02 from "./02";
 
 const solvers = {
   "01": { "1": solverDay01.part1, "2": solverDay01.part2 },
+  "02": { "1": solverDay02.part1, "2": solverDay02.part2 },
 };
 
-export type Day = "01";
+export type Day = "01" | "02";
 export type Part = "1" | "2";
 
 interface Arguments {
