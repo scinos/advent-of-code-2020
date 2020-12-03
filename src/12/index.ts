c762640b906a2f2721b8626d20046279f3c417a1
Sergio Cinos <sergio.cinos@gmail.com>
2020-12-03 06:42:22 +0100

    Scafolding

---
+++ src/12/index.ts
@@ -0,0 +1,9 @@
+import { Solver } from "../run";
+
+export const part1: Solver = () => {
+  throw new Error("Not implemented");
+};
+
+export const part2: Solver = () => {
+  throw new Error("Not implemented");
+};
