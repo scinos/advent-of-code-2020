4e136ba3e67b7ca61bdf8ede4325d2084b3d7a07
Sergio Cinos <sergio.cinos@gmail.com>
2020-12-07 07:05:24 +0100

    Improve day 07

--- src/07/index.ts
+++ src/07/index.ts
@@ -1,39 +1,40 @@
 import { Solver } from "../run";
 
 type Bag = {
-  name: string;
-  bags: Map<string, number>;
+  bags: Map<Bag, number>;
   containedBy: Set<Bag>;
+};
+
+const getBag = (bags: Map<string, Bag>, name: string): Bag => {
+  if (!bags.has(name)) {
+    bags.set(name, {
+      bags: new Map<Bag, number>(),
+      containedBy: new Set<Bag>(),
+    });
+  }
+  return bags.get(name);
 };
 
 const generateBags = (input: string[]): Map<string, Bag> => {
   const bags = new Map<string, Bag>();
   const reBag = /^(?<color>.*?) bags contain (?:no other bags|(?<content>.*))\.$/;
-  const reContent = /^ ?(?<bagAmmount>\d) (?<bagColor>.*?) bags?$/;
+  const reContent = /(?<bagAmmount>\d) (?<bagColor>.*?) bags?(?:, |$)/g;
 
   for (const line of input) {
     const { color, content } = line.match(reBag).groups;
-    const bag: Bag = {
-      name: color,
-      bags: new Map<string, number>(),
-      containedBy: new Set<Bag>(),
-    };
-    bags.set(bag.name, bag);
+    const bag: Bag = getBag(bags, color);
 
     if (content) {
-      const contentBags = content.split(",");
-      for (const contentBag of contentBags) {
-        const { bagAmmount, bagColor } = contentBag.match(reContent).groups;
-        bag.bags.set(bagColor, Number(bagAmmount));
+      for (const {
+        groups: { bagColor, bagAmmount },
+      } of content.matchAll(reContent)) {
+        const subBag = getBag(bags, bagColor);
+        subBag.containedBy.add(bag);
+        bag.bags.set(subBag, Number(bagAmmount));
       }
     }
   }
 
-  for (const bag of bags.values()) {
-    for (const subBag of bag.bags.keys()) {
-      bags.get(subBag).containedBy.add(bag);
-    }
-  }
   return bags;
 };
 
@@ -44,9 +45,8 @@ export const part1: Solver = (input) => {
 
   const extractContainers = (bag: Bag) => {
     for (const subBag of bag.containedBy.values()) {
-      const { name } = subBag;
-      if (!containedBy.has(name)) {
-        containedBy.add(name);
+      if (!containedBy.has(subBag)) {
+        containedBy.add(subBag);
         extractContainers(subBag);
       }
     }
@@ -62,9 +62,10 @@ export const part2: Solver = (input) => {
   const extractContainers = (bag: Bag): number => {
     let total = 1;
     for (const [subBag, ammount] of bag.bags.entries()) {
-      total += ammount * extractContainers(bags.get(subBag));
+      total += ammount * extractContainers(subBag);
     }
     return total;
   };
+
   return String(extractContainers(bags.get("shiny gold")) - 1);
 };
