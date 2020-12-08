0839816022e3586cb1c85f998164059747e27c47
Sergio Cinos <sergio.cinos@gmail.com>
2020-12-08 06:40:36 +0100

    Day 08

--- src/08/index.ts
+++ src/08/index.ts
@@ -1,9 +1,61 @@
 import { Solver } from "../run";
 
-export const part1: Solver = () => {
-  throw new Error("Not implemented");
+const run = (program: string[]) => {
+  let acc = 0;
+  let pointer = 0;
+  const counter = new Array(program.length).fill(0);
+  const re = /^(?<ins>[a-z]{3}) (?<num>[+-]\d+)$/;
+
+  while (pointer < program.length) {
+    if (counter[pointer] >= 1) {
+      throw acc;
+    }
+    counter[pointer]++;
+
+    const { ins, num } = program[pointer].match(re).groups;
+    switch (ins) {
+      case "acc":
+        acc += Number(num);
+        pointer++;
+        break;
+      case "jmp":
+        pointer += Number(num);
+        break;
+      case "nop":
+        pointer++;
+        break;
+    }
+  }
+
+  return acc;
 };
 
-export const part2: Solver = () => {
-  throw new Error("Not implemented");
+export const part1: Solver = (input) => {
+  try {
+    return String(run(input));
+  } catch (e) {
+    return String(e);
+  }
+};
+
+export const part2: Solver = (input) => {
+  const program = [...input];
+
+  for (let i = 0; i < program.length; i++) {
+    if (program[i].startsWith("acc")) continue;
+
+    const originalInstruction = program[i];
+    if (program[i].startsWith("nop")) {
+      program[i] = program[i].replace("nop", "jmp");
+    } else {
+      program[i] = program[i].replace("jmp", "nop");
+    }
+    try {
+      const result = run(program);
+      return String(result);
+    } catch {
+      program[i] = originalInstruction;
+    }
+  }
+  throw new Error("Answer not found");
 };
---
+++ src/08/test.ts
@@ -0,0 +1,35 @@
+import { strict as assert } from "assert";
+import { part1, part2 } from ".";
+import { readInput } from "../lib/readInput";
+
+describe("Day 8", () => {
+  const input = [
+    "nop +0",
+    "acc +1",
+    "jmp +4",
+    "acc +3",
+    "jmp -3",
+    "acc -99",
+    "acc +1",
+    "jmp -4",
+    "acc +6",
+  ];
+
+  it("Part 1", () => {
+    assert.equal(part1(input), "5");
+  });
+  it("Part 2", () => {
+    assert.equal(part2(input), "8");
+  });
+
+  describe("Solutions", () => {
+    it("Part 1", async () => {
+      const input = await readInput("08");
+      assert.equal(part1(input), "1179");
+    });
+    it("Part 2", async () => {
+      const input = await readInput("08");
+      assert.equal(part2(input), "1089");
+    });
+  });
+});
