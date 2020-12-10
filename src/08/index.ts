81de1ced6a77bb60fecca588ccfc8590faf6dec0
Sergio Cinos <sergio.cinos@gmail.com>
2020-12-10 05:57:07 +0100

    Optimize day 8

--- src/08/index.ts
+++ src/08/index.ts
@@ -1,14 +1,18 @@
+/* eslint-disable no-param-reassign */
 import { Solver } from "../run";
 
 const run = (program: string[]) => {
   let acc = 0;
   let pointer = 0;
+  const jmpNop = [];
+
   const counter = new Array(program.length).fill(0);
   const re = /^(?<ins>[a-z]{3}) (?<num>[+-]\d+)$/;
 
   while (pointer < program.length) {
     if (counter[pointer] >= 1) {
-      throw acc;
+      // eslint-disable-next-line no-throw-literal
+      throw { acc, jmpNop };
     }
     counter[pointer]++;
 
@@ -19,43 +23,53 @@ const run = (program: string[]) => {
         pointer++;
         break;
       case "jmp":
+        jmpNop.push(pointer);
         pointer += Number(num);
         break;
       case "nop":
+        jmpNop.push(pointer);
         pointer++;
         break;
     }
   }
 
-  return acc;
+  return { acc, jmpNop };
 };
 
 export const part1: Solver = (input) => {
   try {
-    return String(run(input));
-  } catch (e) {
-    return String(e);
+    return String(run(input).acc);
+  } catch ({ acc }) {
+    return String(acc);
+  }
+};
+
+const toggle = (program: string[], pos: number): void => {
+  if (program[pos].startsWith("nop")) {
+    program[pos] = program[pos].replace("nop", "jmp");
+  } else {
+    program[pos] = program[pos].replace("jmp", "nop");
   }
 };
 
 export const part2: Solver = (input) => {
   const program = [...input];
+  const tried: number[] = [];
 
-  for (let i = 0; i < program.length; i++) {
-    if (program[i].startsWith("acc")) continue;
-
-    const originalInstruction = program[i];
-    if (program[i].startsWith("nop")) {
-      program[i] = program[i].replace("nop", "jmp");
-    } else {
-      program[i] = program[i].replace("jmp", "nop");
-    }
+  while (true) {
     try {
-      const result = run(program);
-      return String(result);
-    } catch {
-      program[i] = originalInstruction;
+      return String(run(program).acc);
+    } catch ({ acc, jmpNop }) {
+      if (tried.length) {
+        toggle(program, tried[tried.length - 1]);
+      }
+
+      let nextInstructionToTry;
+      do {
+        nextInstructionToTry = jmpNop.pop();
+      } while (tried.includes(nextInstructionToTry));
+      tried.push(nextInstructionToTry);
+      toggle(program, nextInstructionToTry);
     }
   }
-  throw new Error("Answer not found");
 };
