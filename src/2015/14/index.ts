c2f196d1540906111a544ef0ec9dab950cd26703
Sergio Cinos <sergio.cinos@gmail.com>
2020-12-29 12:43:28 +0100

    day 14

--- src/2015/14/index.ts
+++ src/2015/14/index.ts
@@ -1,9 +1,100 @@
 import type { Solver } from "../../runner";
 
+// Dancer can fly 27 km/s for 5 seconds, but then must rest for 132 seconds.
+const re = /^(.*?) can fly ([0-9]+) km\/s for ([0-9]+) seconds, but then must rest for ([0-9]+) seconds\.$/;
+
+type Reindeer = {
+  name: string;
+  speed: number;
+  duration: number;
+  rest: number;
+};
+type RaceResult = { name: string; distance: number };
+
+export const extract = (input: string): Reindeer => {
+  const [, name, speed, duration, rest] = re.exec(input)!;
+  return {
+    name,
+    speed: Number(speed),
+    duration: Number(duration),
+    rest: Number(rest),
+  };
+};
+
+export const computeDistance = (
+  raceLength: number,
+  speed: number,
+  duration: number,
+  rest: number
+): number => {
+  const cycleLength = duration + rest;
+  const cycleDistance = speed * duration;
+  const cycles = Math.floor(raceLength / cycleLength);
+  const partialCycle = Math.min(duration, raceLength % cycleLength);
+
+  const distance = cycles * cycleDistance + partialCycle * speed;
+
+  return distance;
+};
+
+export const race = (
+  raceLength: number,
+  reindeers: Reindeer[]
+): RaceResult[] => {
+  return reindeers
+    .map(({ name, speed, duration, rest }) => {
+      return {
+        name,
+        distance: computeDistance(raceLength, speed, duration, rest),
+      };
+    })
+    .sort((a, b) => b.distance - a.distance);
+};
+
+export const racePoints = (
+  raceLength: number,
+  reindeers: Reindeer[]
+): RaceResult => {
+  const scores: Record<string, number> = {};
+
+  reindeers.forEach((r) => {
+    scores[r.name] = 0;
+  });
+
+  for (let i = 1; i <= raceLength; i++) {
+    const partialResult = race(i, reindeers);
+    const partialWinnerDistance = partialResult[0].distance;
+
+    for (let h = 0; h < partialResult.length; h++) {
+      const reindeer = partialResult[h];
+      // In case there are a few reindeers tied in distance
+      if (reindeer.distance === partialWinnerDistance) {
+        scores[reindeer.name]++;
+      }
+    }
+  }
+
+  const winner: RaceResult = { distance: 0, name: "" };
+  for (const [name, distance] of Object.entries(scores)) {
+    if (distance > winner.distance) {
+      winner.distance = distance;
+      winner.name = name;
+    }
+  }
+
+  return winner;
+};
+
 export const part1: Solver = (input) => {
-  throw new Error("Not implemented");
+  const raceLength = 2503;
+  const reindeers = input.map(extract);
+  const winner = race(raceLength, reindeers);
+  return String(winner[0].distance);
 };
 
 export const part2: Solver = (input) => {
-  throw new Error("Not implemented");
+  const raceLength = 2503;
+  const reindeers = input.map(extract);
+  const winner = racePoints(raceLength, reindeers);
+  return String(winner.distance);
 };
---
+++ src/2015/14/test.ts
@@ -0,0 +1,80 @@
+import { strict as assert } from "assert";
+import { part1, part2, extract, computeDistance, race, racePoints } from ".";
+import { readInput } from "../../lib/readInput";
+
+describe("Day 14: Reindeer Olympics", () => {
+  describe("Spec extractor", () => {
+    it("Extracts the specs", () => {
+      const spec =
+        "Dancer can fly 27 km/s for 5 seconds, but then must rest for 132 seconds.";
+      assert.deepEqual(extract(spec), {
+        name: "Dancer",
+        speed: 27,
+        duration: 5,
+        rest: 132,
+      });
+    });
+  });
+
+  describe("Reindeer speed calculator", () => {
+    it("Calculates the speed", () => {
+      // 1 m/s for 1 sec, no rest
+      assert.equal(computeDistance(1000, 1, 1, 0), 1000);
+
+      // 3 m/s for 1 sec, no rest
+      assert.equal(computeDistance(1000, 3, 1, 0), 3000);
+
+      // 1 m/s for 1 sec, 1 sec rest
+      assert.equal(computeDistance(1000, 1, 1, 1), 500);
+
+      // Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+      assert.equal(computeDistance(1000, 14, 10, 127), 1120);
+
+      // Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+      assert.equal(computeDistance(1000, 16, 11, 162), 1056);
+    });
+  });
+
+  describe("Race outcome", () => {
+    it("Calculates the race outcome", () => {
+      const result = race(1000, [
+        { name: "Comet", speed: 14, duration: 10, rest: 127 },
+        { name: "Dancer", speed: 16, duration: 11, rest: 162 },
+      ]);
+      assert.deepEqual(result, [{ name: "Comet", distance: 1120 }]);
+    });
+
+    it("Calculates the race outcomes with duplicates", () => {
+      const result = race(30, [
+        { name: "Comet", speed: 14, duration: 10, rest: 100 },
+        { name: "Dancer", speed: 7, duration: 20, rest: 100 },
+      ]);
+      assert.deepEqual(result, [
+        { name: "Comet", distance: 140 },
+        { name: "Dancer", distance: 140 },
+      ]);
+    });
+  });
+
+  describe("Part 1 - Distance traveled by the winner", () => {
+    it("Input file", async () => {
+      const input = await readInput("2015/14");
+      assert.equal(part1(input), "2640");
+    });
+  });
+
+  describe("Part 2 - Points traveled by the winner", () => {
+    it("Example from the page", () => {
+      const result = racePoints(1000, [
+        { name: "Comet", speed: 14, duration: 10, rest: 127 },
+        { name: "Dancer", speed: 16, duration: 11, rest: 162 },
+      ]);
+      assert.deepEqual(result, { name: "Dancer", distance: 689 });
+    });
+
+    it("Input file", async () => {
+      const input = await readInput("2015/14");
+      assert.equal(part2(input), "1102");
+    });
+  });
+});
